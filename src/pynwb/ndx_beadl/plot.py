Fix bad axis in state probability calculation

src/pynwb/ndx_beadl/plot.py
@@ -363,8 +363,9 @@ def compute_state_transition_matrix(states: Union[StatesTable, pd.DataFrame],
         data=state_transition_count,
         index=pd.Index(data=state_types['state_name'][:], name='from'),
         columns=pd.Index(data=state_types['state_name'][:], name='to'))
-    state_transition_probability_df = state_transition_count_df.divide(state_transition_count_df.sum(axis='rows'),
-                                                                       axis='columns')
+    state_transition_probability_df = state_transition_count_df.divide(
+        state_transition_count_df.sum(axis='columns'),
+        axis='rows')
 
     return state_transition_count_df, state_transition_probability_df
 